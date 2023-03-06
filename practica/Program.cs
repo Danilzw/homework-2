f9b825fcfa3a96f64c8dce738861cba96481fe32
Danil <shwedx@Yandex.ru>
2023-03-06 18:09:32 +0300

    done

---
+++ practica/Program.cs
@@ -0,0 +1,25 @@
+﻿System.Console.WriteLine("Число:");
+int number = int.Parse(Console.ReadLine());
+
+if(number < 1 ^ number > 7)
+{
+    System.Console.WriteLine("Нет такого дня недели");
+}
+
+switch(number)
+{
+    case 1:
+    System.Console.WriteLine("Понедельник");
+    break;
+    case 2:
+    System.Console.WriteLine("Вторник");
+    break;
+    case 3:
+    System.Console.WriteLine("Среда");
+    break;
+}
+
+if(number == 3 && number == 2)
+{
+    System.Console.WriteLine(" - Это выходной");
+}
---
+++ task3/Program.cs
@@ -0,0 +1,19 @@
+﻿//Задача 15: Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+
+string [] days = new string[7] {"Понедельник", "Вторник", "Среда", "Четверг", "Пятница", "Суббота - Выходной", "Воскресенье - Выходной"};
+
+System.Console.Write("Введите число:");
+int ourday = int.Parse(Console.ReadLine());
+
+if(ourday < 1 ^ ourday > 7)
+{
+    System.Console.WriteLine("Нет такого дня недели");
+}
+else
+{
+    System.Console.WriteLine(days[ourday - 1]);
+}
+
+
+
+
